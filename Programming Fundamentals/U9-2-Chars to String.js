feaacee89c1e7afbb5faadb7139230167204b741
mit3v <stanislawmitew@gmail.com>
2023-09-27 10:32:00 +0300

    doing homework

---
+++ Programming Fundamentals/U9-2-Chars to String.js
@@ -0,0 +1,6 @@
+function charToString(char1, char2, char3) {
+
+    console.log(char1 + char2 + char3);
+}
+
+charToString("a", "b", "c");
---
+++ Programming Fundamentals/U9-4-Convert Meters to Kilometers.js
@@ -0,0 +1,6 @@
+function mToKm(meters) {
+    console.log(`${(meters / 1000).toFixed(2)}`);
+
+}
+
+mToKm(798);
